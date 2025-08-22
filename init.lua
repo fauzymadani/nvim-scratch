734362fa5fab3b5467586bd57d11b19b759e8f12
fauzymadani <keperluansekolahfauzy@gmail.com>
2025-08-22 16:49:16 +0700

    feat: new plugin

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 vim.opt.clipboard = "unnamedplus"
 vim.opt.termguicolors = true
 vim.opt.number = true
+vim.opt.wrap = false
 
 vim.opt.rtp:prepend("~/.local/share/nvim/lazy/lazy.nvim")
 
@@ -9,41 +10,40 @@ require("lazy").setup({
   -- Base16 colorscheme
   { "RRethy/base16-nvim" },
   {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
-  { "justinmk/vim-dirvish" },
   { 'echasnovski/mini.statusline', version = false },
+  
+  { 'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' } },
+  {"BurntSushi/ripgrep"},
+  { 'uZer/pywal16.nvim', as = 'pywal16' },
+  {'nvim-tree/nvim-tree.lua'},
+  { 'wakatime/vim-wakatime', lazy = false },
 })
 
 -- Set colorscheme
-vim.cmd("colorscheme base16-black-metal")  
+local pywal16 = require('pywal16')
+pywal16.setup()
 
--- call plugin
-require('mini.statusline').setup()
+-- require('mini.statusline').setup()
 
--- keymap
--- ctrl n for opening dirvish
-vim.keymap.set("n", "<C-n>", function()
-  for _, win in ipairs(vim.api.nvim_list_wins()) do
-    local buf = vim.api.nvim_win_get_buf(win)
-    if vim.bo[buf].filetype == "dirvish" then
-      vim.api.nvim_win_close(win, true) -- 
-      return
-    end
-  end
-  -- if there's none
-  vim.cmd("vsplit")
-  vim.cmd("vertical resize 30")  
-  vim.cmd("Dirvish")           
-end, { desc = "Toggle Dirvish sidebar" })
+-- keymap untuk toggle NvimTree (Ctrl + n)
+vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle NvimTree sidebar" })
 
--- force to open in new window
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "dirvish",
-  callback = function()
-    vim.keymap.set("n", "<CR>", function()
-      local file = vim.fn.expand("<cfile>")
-      vim.cmd("wincmd p")
-      vim.cmd("edit " .. vim.fn.fnameescape(file))
-    end, { buffer = true })
-  end,
+-- telescope keymap
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- disable netrw 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- setup nvim-tree
+require("nvim-tree").setup({
+  sort = { sorter = "case_sensitive" },
+  view = { width = 25 },
+  renderer = { group_empty = true },
+  filters = { dotfiles = true },
 })
 
